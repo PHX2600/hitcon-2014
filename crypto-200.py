bb1b60b20b096ab2b903c259029a481feaad5e6a
AltF4 <altf4@phx2600.org>
2014-08-16 01:18:35 -0700

    Update crypto-200.py

--- crypto-200.py
+++ crypto-200.py
@@ -2,7 +2,7 @@ import socket
 import fractions
 import sympy
 
-x, y, z = symbols('xyz')
+x = sympy.symbols('x')
 HOST = "54.64.40.172"
 PORT = 5454
 
@@ -14,10 +14,12 @@ while 1:
     all = all.split("\n")
     c1 = int(all[1])
     c2 = int(all[2])
-    for i in range(3, 1000):
-        f = x**3-int(c2)
-        g = (x+1)**3-int(c1)
-        print sympy.gcd(f, g, x)
-    //gcd = x - M (find m from expression above) send m
+    f = x**3-int(c1)
+    g = (x+1)**3-int(c2)
+    print sympy.gcd(f, g, x)
+    print f
+    print g
+    print x
+    #gcd = x - M (find m from expression above) send m
     s.send(str(int(1)) + "\n")
     print s.recv(10000)
